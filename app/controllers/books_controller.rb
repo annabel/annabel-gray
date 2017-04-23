8c1367eef31c53e9463550b282df9d59b8c5726b
Annabel Dunstone Gray <annabel.dunstone@gmail.com>
2017-04-23 12:30:54 -0500

    Bump Rails version; organize books into years

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,32 @@
 class BooksController < ApplicationController
   def index
+    @available_years = Dir.glob(Rails.root.join('app', 'books', '*'))
+                         .select { |path| File.directory?(path) }
+                         .map { |path| path.split('/').last }
+                         .reverse
+
+    year_param = ActionController::Base.helpers.sanitize(params[:year])
+    @current_year =
+      if @available_years.include?(year_param)
+        year_param
+      else
+        Time.zone.now.year.to_s
+      end
+
     @books = []
-    Dir.glob(Rails.root.join('app', 'books', '*.yml')) do |book_file|
+    Dir.glob(Rails.root.join('app', 'books', @current_year, '*.yml')) do |book_file|
       @books << YAML.load_file(book_file)
     end
     @books.sort_by! { |book| Date.parse(book['date']) }.reverse!
   end
 
-  def show
-    @book = YAML.load_file(
-      Rails.root.join('app', 'books', "#{params[:id]}.yml")
-    )
-  end
+  # def show
+  #   title = ActionController::Base.helpers.sanitize(params[:id])
+  #   file_path = Rails.root.join('app', 'books', "#{title}.yml")
+  #   if File.exists?(file_path)
+  #     @book = YAML.load_file(file_path)
+  #   else
+  #     raise ActionController::RoutingError, 'Not Found'
+  #   end
+  # end
 end
